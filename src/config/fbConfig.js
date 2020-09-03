f3fce08fe404c6af2c34b1d0cc581714cb020af4
Huseyin Gundogdu <hgundogdu53@gmail.com>
2020-09-02 20:04:31 -0700

    dotenv added

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -3,14 +3,14 @@ import "firebase/auth";
 import "firebase/firestore"; // <- needed if using firestore
 
 var firebaseConfig = {
-  apiKey: "AIzaSyAC4ZjjOfXsywmVdZsoc7vD6hWlzELthqQ",
-  authDomain: "notion-planner.firebaseapp.com",
-  databaseURL: "https://notion-planner.firebaseio.com",
-  projectId: "notion-planner",
-  storageBucket: "notion-planner.appspot.com",
-  messagingSenderId: "505163193581",
-  appId: "1:505163193581:web:18733186bc9a553c2480e4",
-  measurementId: "G-8SV01HHGNN",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 // Initialize Firebase
